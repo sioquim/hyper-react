198bc55eb0f08ae419fd76b2f8843cf6a0f41305
Franco Sioquim <francosioquim@gmail.com>
2019-05-10 21:42:46 +1200

    build: initial Neutrino files

---
+++ .neutrinorc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  use: [
+    '@neutrinojs/airbnb',
+    [
+      '@neutrinojs/react',
+      {
+        html: {
+          title: 'super-react'
+        }
+      }
+    ],
+    '@neutrinojs/jest'
+  ]
+};
